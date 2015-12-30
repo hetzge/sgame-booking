dde0784d23f919de66109dd8a38d38f36ccdd9c6
hetzge <hetzge@gmx.de>
2015-12-30 23:18:09 +0100

    Without limit fixed

--- src/de/hetzge/sgame/booking/ContainerWithoutLimit.java
+++ src/de/hetzge/sgame/booking/ContainerWithoutLimit.java
@@ -2,4 +2,14 @@ package de.hetzge.sgame.booking;
 
 public class ContainerWithoutLimit<ITEM> extends Container<ITEM> {
 
+	@Override
+	public synchronized boolean can(ITEM item) {
+		return true;
+	}
+
+	@Override
+	public synchronized boolean canAddAmount(ITEM item, int amount) {
+		return true;
+	}
+
 }
